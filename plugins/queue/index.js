78a947aa282f41fb3b177ae92ac37923286bb904
ppaul <pritam.paul@verizonmedia.com>
2020-02-15 02:00:38 +0530

    feat: Add stats api

--- plugins/queue/index.js
+++ plugins/queue/index.js
@@ -1,13 +1,15 @@
 'use strict';
 const putRoute = require('./put');
 const deleteRoute = require('./delete');
+const statsRoute = require('./stats');
 
 const queuePlugin = {
     name: 'queue',
     register: async function (server, options) {
         server.route([
             putRoute(),
-            deleteRoute()
+            deleteRoute(),
+            statsRoute()
         ]);
     }
 };
