ebdeb940bfc06a3eb18893d4a44fbe0a30519354
Teppei Minegishi <saka2tetsu@gmail.com>
2020-10-19 13:54:36 +0900

    fix(2240): Convert SCHEDULER_ENABLED value from string to boolean. (#23)
    * fix: convert to boolean from string
    
    * fix: convertToBool function
    
    * Revert "fix: convertToBool function"
    
    This reverts commit 627e79793742a6941a90a5b2a7ea89775dc6e33e.
    
    * Revert "fix: convert to boolean from string"
    
    This reverts commit 215ebbbce4c7620b42c93c08e3680202fdb8527a.
    
    * fix: add format to SCHEDULER_ENABLED env
    
    * fix: change to space from tab
    
    * include multiple revert
    
    Revert "fix: change to space from tab"
    
    This reverts commit dedad4775986fb28e92f7a41133433bee6628aee.
    
    Revert "fix: add format to SCHEDULER_ENABLED env"
    
    This reverts commit c16a854c5e80ddd651d11502f8abb1d831ed8779.
    
    Revert "Revert "fix: convert to boolean from string""
    
    This reverts commit 5eec9e211de2bdbc4abd42cc0a4deccfde7b2366.
    
    Revert "Revert "fix: convertToBool function""
    
    This reverts commit 814f22c7343669636614a7f7cb3fcacca8a37a5b.
    
    * fix: add a true list based on yaml definition
    
    Co-authored-by: kkai <kkai@yahoo-corp.jp>
    Co-authored-by: Yuichi Sawada <4645011+yuichi10@users.noreply.github.com>
    Co-authored-by: sugarnaoming <sakigithub@gmail.com>

--- config/rabbitmq.js
+++ config/rabbitmq.js
@@ -2,10 +2,28 @@
 
 const config = require('config');
 
+/**
+ * convert value to Boolean
+ * @method convertToBool
+ * @param {(Boolean|String)} value
+ * @return {Boolean}
+ */
+function convertToBool(value) {
+    if (typeof value === 'boolean') {
+        return value;
+    }
+
+    // trueList refers to https://yaml.org/type/bool.html
+    const trueList = ['on', 'true', 'yes', 'y'];
+    const lowerValue = String(value).toLowerCase();
+
+    return trueList.includes(lowerValue);
+}
+
 const rabbitmqConfig = config.get('scheduler').rabbitmq;
 const { protocol, username, password, host, port, exchange, vhost, connectOptions } = rabbitmqConfig;
 const amqpURI = `${protocol}://${username}:${password}@${host}:${port}${vhost}`;
-const schedulerMode = config.get('scheduler').enabled;
+const schedulerMode = convertToBool(config.get('scheduler').enabled);
 
 /**
  * get configurations for rabbitmq
