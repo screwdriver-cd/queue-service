4f756db7676acba5a5b608d28ba4a652bf345bb2
Haruka Kawamura <itleigns@gmail.com>
2023-08-04 12:31:02 +0900

    Update cron.js

--- plugins/queue/utils/cron.js
+++ plugins/queue/utils/cron.js
@@ -109,7 +109,8 @@ const transformCron = (cronExp, jobId) => {
 const nextExecution = cronExp => {
     // Scheduled jobs may run a little ahead of schedule.
     // The next job to be executed is delayed so that it will be at the next timing even in that case.
-    let date = Date.now();
+    const date = Date.now();
+    
     date.setMinutes(date.getMinutes() + 5);
     const options = {
         currentDate: date
